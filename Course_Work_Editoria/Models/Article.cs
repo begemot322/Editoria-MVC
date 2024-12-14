01861ff8b28e046cc1ef9b04eeef8b6ef0e3971b
Николай <kolya.zaitsev6163@gmail.com>
2024-12-14 18:13:46 +0400

    Add validation

--- Course_Work_Editoria/Models/Article.cs
+++ Course_Work_Editoria/Models/Article.cs
@@ -7,8 +7,16 @@ namespace Course_Work_Editoria.Models
     {
         [Key]
         public int ArticleId { get; set; }
+        [Required(ErrorMessage = "Укажите название статьи")]
+        [StringLength(100, ErrorMessage = "Длина названия не должна превышать 100 символов")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Введите текст статьи")]
         public string Text { get; set; }
+
+        [Required(ErrorMessage = "Укажите дату публикации")]
+        [DataType(DataType.Date, ErrorMessage = "Неверный формат даты")]
+        [Display(Name = "Дата публикации")]
         public DateTime PublicationDate { get; set; }
 
         // Связь с выпуском
