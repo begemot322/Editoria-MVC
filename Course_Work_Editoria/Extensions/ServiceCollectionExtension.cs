38b9d1404e2555d8355a97e4ab9243ca0bdb0c0e
Николай <kolya.zaitsev6163@gmail.com>
2025-01-18 18:24:15 +0400

    add Extensions

---
+++ Course_Work_Editoria/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,31 @@
+﻿using Editoria.Data.Context;
+using Editoria.Data.Repository.IRepository;
+using Editoria.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Course_Work_Editoria.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static WebApplicationBuilder AddDatabaseServices(this WebApplicationBuilder builder)
+        {
+            var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connString));
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder AddRepositoryServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IAdvertisementRepository, AdvertisementRepository>();
+            builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
+            builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            builder.Services.AddScoped<IEditorRepository, EditorRepository>();
+            builder.Services.AddScoped<IIssueRepository, IssueRepository>();
+            builder.Services.AddScoped<INewspaperRepository, NewspaperRepository>();
+
+            return builder;
+        }
+    }
+}
--- Course_Work_Editoria/Program.cs
+++ Course_Work_Editoria/Program.cs
@@ -1,33 +1,18 @@
-using Editoria.Data.Context;
-using Editoria.Data.Repository.IRepository;
-using Editoria.Data.Repository;
-using Microsoft.EntityFrameworkCore;
+using Course_Work_Editoria.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var connString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(connString));
-
-//Repositories
-builder.Services.AddScoped<IAdvertisementRepository, AdvertisementRepository>();
-builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
-builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IEditorRepository, EditorRepository>();
-builder.Services.AddScoped<IIssueRepository, IssueRepository>();
-builder.Services.AddScoped<INewspaperRepository, NewspaperRepository>();
+builder
+    .AddDatabaseServices()
+    .AddRepositoryServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
