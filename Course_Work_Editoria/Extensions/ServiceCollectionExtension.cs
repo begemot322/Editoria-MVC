905bb6416ad98d1a7efa998b38d7f3e3668a617b
Николай <kolya.zaitsev6163@gmail.com>
2025-01-24 11:06:21 +0400

    add extension

--- Course_Work_Editoria/Extensions/ServiceCollectionExtension.cs
+++ Course_Work_Editoria/Extensions/ServiceCollectionExtension.cs
@@ -2,6 +2,13 @@
 using Editoria.Data.Repository.IRepository;
 using Editoria.Data.Repository;
 using Microsoft.EntityFrameworkCore;
+using Course_Work_Editoria.Authentication.Interfaces;
+using Course_Work_Editoria.Authentication;
+using Course_Work_Editoria.Authentication.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
 
 namespace Course_Work_Editoria.Extensions
 {
@@ -24,8 +31,63 @@ namespace Course_Work_Editoria.Extensions
             builder.Services.AddScoped<IEditorRepository, EditorRepository>();
             builder.Services.AddScoped<IIssueRepository, IssueRepository>();
             builder.Services.AddScoped<INewspaperRepository, NewspaperRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
 
             return builder;
+        }
+        public static WebApplicationBuilder AddJwtServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(nameof(JwtOptions)));
+            builder.Services.AddScoped<IJwtProvider, JwtProvider>();
+
+            return builder;
+        }
+        public static WebApplicationBuilder AddApplicationServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<UserService>();
+            builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+            return builder;
+        }
+        public static WebApplicationBuilder AddAuthenticationServices(this WebApplicationBuilder builder)
+        {
+            var configuration = builder.Configuration;
+            var jwtOptions = configuration.GetSection(nameof(JwtOptions)).Get<JwtOptions>();
+
+            builder.Services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.SaveToken = true;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions!.SecretKey)),
+                        RoleClaimType = ClaimTypes.Role,
+                    };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            context.Token = context.Request.Cookies["SecurityCookies"];
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            builder.Services.AddAuthorization();
+
+            return builder;
+
         }
     }
 }
