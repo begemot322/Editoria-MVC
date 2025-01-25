1b4f7b1af0687408cfc8776ba5a57d536b1f4c9c
Николай <kolya.zaitsev6163@gmail.com>
2025-01-25 12:49:53 +0400

    add new entity

---
+++ Editoria.Models/Entities/Tag.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editoria.Models.Entities
+{
+    public class Tag
+    {
+        [Key]
+        public int TagId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; } = "Без описания";
+
+        // Связь многие-ко-многим со статьями
+        [ValidateNever]
+        public ICollection<ArticleTag> ArticleTags { get; set; }
+    }
+}
