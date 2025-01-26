c61f1e153ea75d70616251fff717459a321aeea1
Николай <kolya.zaitsev6163@gmail.com>
2025-01-26 13:14:50 +0400

    add view model

---
+++ Editoria.Models/ViewModel/RegisterUserRequestVM.cs
@@ -0,0 +1,18 @@
+﻿using Editoria.Models.Requests;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editoria.Models.ViewModel
+{
+    public class RegisterUserRequestVM
+    {
+        public RegisterUserRequest RegisterUserRequest { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> Roles { get; set; }
+    }
+}
