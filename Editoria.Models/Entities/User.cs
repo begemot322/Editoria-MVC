f6dbab0a205af3c28397e03b16bad743ca880524
Николай <kolya.zaitsev6163@gmail.com>
2025-01-26 13:14:37 +0400

    add role

--- Editoria.Models/Entities/User.cs
+++ Editoria.Models/Entities/User.cs
@@ -14,6 +14,7 @@ namespace Editoria.Models.Entities
         public string PasswordHash { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public string Role { get; set; }
         public string? ImageUrl { get; set; }
     }
 }
