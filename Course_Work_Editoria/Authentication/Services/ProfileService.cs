2ce4f5635f7dbbef63929b5f98e4134c0650dab6
Николай <kolya.zaitsev6163@gmail.com>
2025-01-26 21:27:36 +0400

    create profile service and removed change password

---
+++ Course_Work_Editoria/Authentication/Services/ProfileService.cs
@@ -0,0 +1,46 @@
+﻿using Course_Work_Editoria.Authentication.Interfaces;
+using Editoria.Data.Repository.IRepository;
+using Editoria.Models.Entities;
+
+namespace Course_Work_Editoria.Authentication.Services
+{
+    public class ProfileService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+
+        public ProfileService(IUserRepository userRepository, IPasswordHasher passwordHasher)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+        }
+
+        public void ChangePassword(Guid userId, string currentPassword, string newPassword, string confirmPassword)
+        {
+            if (newPassword != confirmPassword)
+            {
+                throw new Exception("Ваши пароли не совпадают.");
+            }
+
+            var user = _userRepository.GetById(userId);
+
+            if (user == null)
+            {
+                throw new Exception("Пользователь не найден.");
+            }
+
+            var isPasswordValid = _passwordHasher.Verify(currentPassword, user.PasswordHash);
+
+            if (isPasswordValid == false)
+            {
+                throw new Exception("Неверный текущий пароль.");
+            }
+
+            var newHashedPassword = _passwordHasher.Generate(newPassword);
+
+            user.PasswordHash = newHashedPassword;
+
+            _userRepository.Update(user);
+        }
+    }
+}
