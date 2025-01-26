e8f819afac1901254ccdcbfa6f5798ccc89540ae
Николай <kolya.zaitsev6163@gmail.com>
2025-01-26 13:14:09 +0400

    add tag helper for checking policy

---
+++ Course_Work_Editoria/TagHelpers/PolicyTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Security.Claims;
+
+namespace Course_Work_Editoria.TagHelpers
+{
+    [HtmlTargetElement(Attributes ="policy")]
+    public class PolicyTagHelper : TagHelper
+    {
+        private readonly IAuthorizationService _authService;
+        private readonly ClaimsPrincipal _principal;
+
+        public PolicyTagHelper(IAuthorizationService authService, IHttpContextAccessor httpContextAccessor)
+        {
+            _authService = authService;
+            _principal = httpContextAccessor.HttpContext.User;
+        }
+        public string Policy { get; set; }
+       
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if(!(await _authService.AuthorizeAsync(_principal, Policy)).Succeeded)
+                output.SuppressOutput();
+        }
+    }
+}
