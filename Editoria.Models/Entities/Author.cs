13bd9d6a75c705189aeb5ae04011348e1f8b24fc
Николай <kolya.zaitsev6163@gmail.com>
2025-01-28 09:42:43 +0400

    delete display name

--- Editoria.Models/Entities/Author.cs
+++ Editoria.Models/Entities/Author.cs
@@ -11,19 +11,18 @@ namespace Editoria.Models.Entities
 
         [Required(ErrorMessage = "Имя обязательно для заполнения.")]
         [StringLength(50, ErrorMessage = "Имя не должно превышать 50 символов.")]
-        [DisplayName("Имя")]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "Фамилия обязательна для заполнения.")]
         [StringLength(50, ErrorMessage = "Фамилия не должна превышать 50 символов.")]
-        [DisplayName("Фамилия")]
         public string Surname { get; set; }
+
         [Required(ErrorMessage = "Электронная почта обязательна для заполнения.")]
         [EmailAddress(ErrorMessage = "Введите корректный адрес электронной почты.")]
-        [DisplayName("Почта")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "Номер телефона обязателен для заполнения.")]
         [Phone(ErrorMessage = "Введите корректный номер телефона.")]
-        [DisplayName("Телефон")]
         public string Phone { get; set; }
 
         // Связь со статьями
