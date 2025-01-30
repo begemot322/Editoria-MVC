0d8477156f1706ad6f150c90c5253ead0b5fb7c8
Николай <kolya.zaitsev6163@gmail.com>
2025-01-30 23:38:00 +0400

    add logic for main page

--- Course_Work_Editoria/Controllers/HomeController.cs
+++ Course_Work_Editoria/Controllers/HomeController.cs
@@ -1,22 +1,36 @@
-using System.Diagnostics;
+ using System.Diagnostics;
+using Editoria.Data.Context;
+using Editoria.Data.Repository.IRepository;
+using Editoria.Models.Entities;
 using Editoria.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Course_Work_Editoria.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IArticleRepository _articleRepository;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IArticleRepository articleRepository)
         {
+            _articleRepository = articleRepository;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var articlesWithNewspapers = _articleRepository.GetAllArticles();
+            return View(articlesWithNewspapers);
         }
+        public IActionResult Details(int articleId)
+        {
+            var article = _articleRepository.GetArticleById(articleId);
+            return View(article);
+        }
+
 
         public IActionResult Privacy()
         {
