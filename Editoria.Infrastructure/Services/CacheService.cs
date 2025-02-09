d663882f7eab377e54c308b9b2362f93e2d1c0a0
Николай <kolya.zaitsev6163@gmail.com>
2025-02-09 21:47:43 +0400

    add services

---
+++ Editoria.Infrastructure/Services/CacheService.cs
@@ -0,0 +1,28 @@
+﻿using Editoria.Application.Common.Interfaces.Services;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Editoria.Infrastructure.Services
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IMemoryCache _cache;
+        public CacheService(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+        public T Get<T>(string key)
+        {
+            return _cache.TryGetValue(key, out T value) ? value : default;
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+
+        public void Set<T>(string key, T value, TimeSpan expiration)
+        {
+            _cache.Set(key, value, expiration);
+        }
+    }
+}
---
+++ Editoria.Infrastructure/Services/FileService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Course_Work_Editoria.Services.File
+{
+    public class FileService : IFileService
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public string SaveFile(IFormFile file, string folder)
+        {
+            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            string filePath = Path.Combine(_webHostEnvironment.WebRootPath, folder, fileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            return $"/{folder}/{fileName}";
+        }
+        public void DeleteFile(string relativePath)
+        {
+            string fullPath = Path.Combine(_webHostEnvironment.WebRootPath, relativePath.TrimStart('/'));
+
+            if (System.IO.File.Exists(fullPath))
+            {
+                System.IO.File.Delete(fullPath);
+            }
+        }
+    }
+}
