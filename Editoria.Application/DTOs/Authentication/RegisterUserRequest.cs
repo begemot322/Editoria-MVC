c016162df96c64293f3df388d146d236991b2f8f
Николай <kolya.zaitsev6163@gmail.com>
2025-02-09 21:45:27 +0400

    add dtos request

---
+++ Editoria.Application/DTOs/Authentication/RegisterUserRequest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editoria.Application.DTOs.Authentication
+{
+    public class RegisterUserRequest
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Email обязателен.")]
+        [EmailAddress(ErrorMessage = "Некорректный формат Email.")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Телефон обязателен.")]
+        [Phone(ErrorMessage = "Введите корректный номер телефона.")]
+        public string PhoneNumber { get; set; }
+
+        public string Role { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
